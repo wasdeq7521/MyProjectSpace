bb0d7833252f91b6c5d1d3905f719387572e73da
wasdeq7521 <602441756@qq.com>
2017-08-25 13:34:29 +0800

    收到消息

--- Test/src/test/ch1/HelloGitHub.java
+++ Test/src/test/ch1/HelloGitHub.java
@@ -4,5 +4,6 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("say: hello GitHub !");
 		System.out.println("say: From GitHub !");
+		System.out.println("received");
 	}
 }

62779542c81da45258f4e712b3ed8630481905a6
wasdeq7521 <602441756@qq.com>
2017-08-25 11:45:39 +0800

    Update HelloGitHub.java
    在GitHub上对上传的文件作了修改，测试下载功能

--- Test/src/test/ch1/HelloGitHub.java
+++ Test/src/test/ch1/HelloGitHub.java
@@ -3,5 +3,6 @@ package test.ch1;
 public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("say: hello GitHub !");
+		System.out.println("say: From GitHub !");
 	}
 }

364b495647aba4daf384cafd09cd9465daf51319
wasdeq7521 <602441756@qq.com>
2017-10-09 17:30:36 +0800

    删除Test项目

--- Test/src/test/ch1/HelloGitHub.java
+++
@@ -1,9 +0,0 @@
-package test.ch1;
-
-public class HelloGitHub {
-	public static void main(String[] args) {
-		System.out.println("say: hello GitHub !");
-		System.out.println("say: From GitHub !");
-		System.out.println("received");
-	}
-}
